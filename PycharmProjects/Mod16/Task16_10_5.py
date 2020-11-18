a73d770b2ba6e6bf2f66bebabdec99e9f44aeac7
Alexander <70514174+Sanich11@users.noreply.github.com>
2020-11-18 17:04:58 +0300

    123

---
+++ PycharmProjects/Mod16/Task16_10_5.py
@@ -0,0 +1,8 @@
+class NonPositiveDigitException(ValueError):
+    pass
+
+
+class Square:
+    def __init__(self, a):
+        if a < 0:
+            raise NonPositiveDigitException('Неправильно указанна сторона квадрата')
