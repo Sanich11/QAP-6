eba2c3f524897bebbbc5f6aecc713540aec0cff4
Alexander <70514174+Sanich11@users.noreply.github.com>
2020-12-02 01:15:03 +0300

    upgrade bot

--- PycharmProjects/Mod18/app.py
+++ PycharmProjects/Mod18/app.py
@@ -5,9 +5,32 @@ TOKEN = '1419509223:AAElLhdtLugQVBy9KsdBD_lclCj-0i0Lo5g'
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'hello')
+keys = {
+    'рубль': 'RUR',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'юань': 'CNY',
+    'японская иена': 'JPY',
+    'фунт стерлингов': 'GBP',
+    'гривна': 'UAH',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH'
+}
 
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу, введите команду боту в следующем формате:\n \
+<имя валюты>\n \
+<в какую валюту перевести>\n \
+<количество переводимой валюты>\n Увидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key))
+    bot.reply_to(message, text)
 
 bot.polling()
